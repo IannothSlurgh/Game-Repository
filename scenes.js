ce76f11f0289e9b0567b783285499b12a990d81e
steven bierwagen <sbierwagen@neo.tamu.edu>
2014-03-27 16:10:46 -0500

    changed { to (

--- scenes.js
+++ scenes.js
@@ -412,13 +412,14 @@ Crafty.scene('ConnectionRoom', function(){
         $('#status').text(message);
         $('#login').attr('disabled', false);
       });
-	}
-	socket.on{
+	  
+	socket.on(
 		'mazeDataMsg',
 		function(message){
 			mazeData = JSON.parse(message).mazeArray;
 		});
 	}
+	
     // If a login_ok message is received, proceed to the chat section.
     socket.on(
       'login_ok',
