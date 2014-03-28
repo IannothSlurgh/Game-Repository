9880dd7e0c4a653f5e2c0e35862f4ea687df3549
jacobcreech <mrjacobcreech@gmail.com>
2014-03-28 17:31:59 -0500

    Added removeDuplicates

--- server.js
+++ server.js
@@ -243,7 +243,20 @@ io.sockets.on(
             });
         }
       });
-	  
+	  function removeDuplicates(arr){
+		var i,
+		len = arr.length,
+		out = [],
+		obj = {};
+		
+		for (i = 0; i < len; i++){
+			obj[arr[i]] = 0;
+		}
+		for (i in obj){
+			out.push(i);
+		}
+		return out;
+	  }
 	client.on(
 	  'userlist',
 	  function(message){
