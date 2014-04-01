70602cc92d8899b05bcdde72dbb336d931c15a0c
jacobcreech <mrjacobcreech@gmail.com>
2014-03-31 21:38:18 -0500

    Update components.js
    Attempted Fix

--- components.js
+++ components.js
@@ -1,69 +1,54 @@
 //Grid component allows an element to be Located
-Crafty.c('Grid', 
-{
-	init: function() 
-	{
+Crafty.c('Grid', {
+	init: function() {
 		this.attr({
 			w: Game.map_grid.tile.width,
 			h: Game.map_grid.tile.height
 		});
 	},
-	
+
 	//Locate this entity at the given position on grid
-	at: function(x, y)
-	{
-		if(x === undefined && y === undefined)
-		{
+	at: function(x, y){
+		if(x === undefined && y === undefined){
 			return {x: this.x/Game.map_grid.tile.width,
 				y: this.y/Game.map_grid.tile.height};
 		}
-		else
-		{
-			this.attr({
-			x: x * Game.map_grid.tile.width, 
-			y: y * Game.map_grid.tile.height
-			});
+		else{
+			this.attr({x: x * Game.map_grid.tile.width, 
+				y: y * Game.map_grid.tile.height});
 			return this;
 		}
 	}
 });
 
 //entity that is drawn in 2D on canvas via Grid
-Crafty.c('Actor', 
-{
-	init: function() 
-	{
+Crafty.c('Actor', {
+	init: function() {
 		this.requires('2D, Canvas, Grid');
 	},
 });
 
 //Tree is an Actor with a certain color
-Crafty.c('Wall', 
-{
-	init: function() 
-	{
+Crafty.c('Wall', {
+	init: function() {
 		this.requires('Actor, Color, Solid')
 			.color('rgb(20, 185, 40)');
 	},
 });
 
-Crafty.c('EnemyPlayer', 
-{
-	init: function() 
-	{
+Crafty.c('EnemyPlayer', {
+	init: function() {
 		this.attr({
 			index: undefined
 		});
-		
+
 		this.requires('Actor, Color')
 			.color('rgb(223, 0, 255)');
 	},
 });
 
-Crafty.c('Path', 
-{
-	init: function()
-	{
+Crafty.c('Path', {
+	init: function(){
 		this.requires('Actor, Color')
 			.color('rgb(249, 223, 125)');
 	},
@@ -71,15 +56,13 @@ Crafty.c('Path',
 
 
 
-Crafty.c('PlayerCharacter', 
-{
-	init: function()
-	{
+Crafty.c('PlayerCharacter', {
+	init: function(){
 		this.attr({
 			score: 0,
 			index: player_number
 		});
-		
+
 		this.requires('Actor, Fourway, Color, Collision')
 			.fourway(4)
 			.color('rgb(20, 75, 40)')
@@ -90,40 +73,35 @@ Crafty.c('PlayerCharacter',
 				Crafty.trigger('PlayerMoved');
 			});
 	},
-	
+
 	//registers a stop-movement when entity hits an entity
 	//with the "Solid" component
-	stopOnSolids: function() 
-	{
+	stopOnSolids: function() {
 		this.onHit('Solid', this.stopMovement);
 		return this;
 	},
-	
+
 	//stops movement
-	stopMovement: function()
-	{
+	stopMovement: function(){
 		this._speed = 0;
 		if(this._movement){
 			this.x -= this._movement.x;
 			this.y -= this._movement.y;
 		}
 	},
-	
-	collectMoney: function(data)
-	{
+
+	collectMoney: function(data){
 		var money = data[0].obj;
 		this.score += money.value;
-		
+
 		//send signal to the server indicating that money has been collected
 		Crafty.trigger('MoneyCollected', money);
 		Crafty.trigger('ChangeScore', this);
 	}
 });
 
-Crafty.c('Money', 
-{
-	init: function()
-	{
+Crafty.c('Money', {
+	init: function(){
 		this.attr({
 			array_index : -1,
 			value: 50
@@ -133,125 +111,104 @@ Crafty.c('Money',
 	}
 });
 
-Crafty.c('2PlayerButton', 
-{
-	init: function()
-	{
+Crafty.c('2PlayerButton', {
+	init: function(){
 		this.requires('2D, Canvas, Image, Mouse')
 		.image("https://raw.githubusercontent.com/IannothSlurgh/Game-Repository/master/2_player_unselected.png")
-		.bind('MouseOver', function(e)
-		{
+		.bind('MouseOver', function(e){
 			this.image("https://raw.githubusercontent.com/IannothSlurgh/Game-Repository/master/2_player_selected.png");
 		})
-		.bind('MouseOut', function(e)
-		{
+		.bind('MouseOut', function(e){
 			this.image("https://raw.githubusercontent.com/IannothSlurgh/Game-Repository/master/2_player_unselected.png");
 		})
-		.bind('Click', function(e)
-		{
+		.bind('Click', function(e){
 			is_two_player_game = true;
 			Crafty.scene('ConnectionRoom');
 		});
 	}
-	
+
 });
 
-Crafty.c('4PlayerButton', 
-{
-	init: function()
-	{
+Crafty.c('4PlayerButton', {
+	init: function(){
 		this.requires('2D, Canvas, Image, Mouse')
-		.image("https://raw.githubusercontent.com/IannothSlurgh/Game-Repository/master/4_player_unselected.png");
-		.bind('MouseOver', function(e)
-		{
-			this.image("https://raw.githubusercontent.com/IannothSlurgh/Game-Repository/master/4_player_selected.png");
+		.image("https://raw.githubusercontent.com/IannothSlurgh/Game-Repository/master/4_player_unselected.png")
+		.bind('MouseOver', function(e){
+			this.image("https://raw.githubusercontent.com/IannothSlurgh/Game-Repository/master/4_player_selected.png")
 		})
-		.bind('MouseOut', function(e)
-		{
+		.bind('MouseOut', function(e){
 			this.image("https://raw.githubusercontent.com/IannothSlurgh/Game-Repository/master/4_player_unselected.png");
 		})
-		.bind('Click', function(e)
-		{
+		.bind('Click', function(e){
 			is_two_player_game = false;
 			Crafty.scene('ConnectionRoom');
 		});
 	}
+
 });
 
-Crafty.c('HelpButton', 
-{
-	init: function()
-	{
+Crafty.c('HelpButton', {
+	init: function(){
 		this.requires('2D, Canvas, Image, Mouse')
 		.image("https://raw.githubusercontent.com/IannothSlurgh/Game-Repository/master/help_button_unselected.png")
-		.bind('MouseOver', function(e)
-		{
-			this.image("https://raw.githubusercontent.com/IannothSlurgh/Game-Repository/master/help_button_selected.png");
+		.bind('MouseOver', function(e){
+			this.image("https://raw.githubusercontent.com/IannothSlurgh/Game-Repository/master/help_button_selected.png")
 		})
 		.bind('MouseOut', function(e){
 			this.image("https://raw.githubusercontent.com/IannothSlurgh/Game-Repository/master/help_button_unselected.png");
 		})
-		.bind('Click', function(e)
-		{
+		.bind('Click', function(e){
 			Crafty.scene('Help');
 		});
 	}
-	
+
 });
 
-Crafty.c('MineCart', 
-{
-	init: function()
-	{
+Crafty.c('MineCart', {
+	init: function(){
 		this.requires('2D, Canvas, Image, Tween')
 			.image("https://raw.githubusercontent.com/IannothSlurgh/Game-Repository/master/new_creepy_cart.png")
 			.tween({x: 880}, 5000)
-			.bind('TweenEnd', function()
-			{
-				
+			.bind('TweenEnd', function(){
+
 				var track_num = Math.floor(Math.random() * 3);
 				var direction = Math.round(Math.random());
 				console.log(track_num);
 				switch(track_num)
 				{
-					case 0:
-						this.y = 201; 
-						this.x = -100;
-						this.tween({x: 880}, 5000);
-						break;
-					case 1: 
-						this.y = 357; 
-						this.x = 880;
-						this.tween({x: 0 - 126}, 5000);
-						break;
-					case 2: 
-						this.y = 513; 
-						this.x = -100;
-						this.tween({x: 880}, 5000);
-						break;
+				case 0:
+					this.y = 201; 
+					this.x = -100;
+					this.tween({x: 880}, 5000);
+					break;
+				case 1: 
+					this.y = 357; 
+					this.x = 880;
+					this.tween({x: 0 - 126}, 5000);
+					break;
+				case 2: 
+					this.y = 513; 
+					this.x = -100;
+					this.tween({x: 880}, 5000);
+					break;
 				}
 			});
 	}
 });
 
-Crafty.c('StartButton', 
-{
-	init: function()
-	{
+Crafty.c('StartButton', {
+	init: function(){
 		this.requires('2D, Canvas, Color, Mouse')
 		.color('rgb(0, 255, 255)')
-		.bind('MouseOver', function(e)
-		{
+		.bind('MouseOver', function(e){
 			this.color('rgb(64, 224, 208)')
 		})
-		.bind('MouseOut', function(e)
-		{
+		.bind('MouseOut', function(e){
 			this.color('rgb(0, 255, 255)');
 		})
-		.bind('Click', function(e)
-		{
+		.bind('Click', function(e){
 			//To Be Implemented
 		});
 	}
-	
+
 });
