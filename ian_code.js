dc4b893eedfd4e09c185bf23f0ac460a71a740e9
IannothSlurgh <Slurgh2007@hotmail.com>
2014-04-06 02:39:03 -0500

    Forgot to post shadow image to imgur

--- ian_code.js
+++ ian_code.js
@@ -87,7 +87,7 @@ Crafty.scene('Phase 3', function()
 						var shadow_id = "shadow" + shadow_num.toString();
 						var left = getAbsoluteFromGrid(far_left);
 						var top = getAbsoluteFromGrid(far_top);
-						addImage(shadow_id, src, left, top, 9, 44, 44);
+						addImage(shadow_id, "http://i.imgur.com/Pa1TQOw.png", left, top, 9, 44, 44);
 						var shadow = getElementById(shadow_id);
 						shadow.style.opacity = ".4";
 						//Both of these functions use a 'generator pattern' to create
